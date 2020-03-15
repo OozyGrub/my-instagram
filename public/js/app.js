411c6d530abeb56870dd0818d35cb6b2bca50cdc
OOZYGRUB <OozyGrub@hotmail.com>
2020-03-15 09:22:45 +0700

    Change Projecet Name

--- public/js/app.js
+++ public/js/app.js
@@ -49660,8 +49660,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/kritkruaykitanon/lol/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/kritkruaykitanon/lol/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/kritkruaykitanon/myInstagram/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/kritkruaykitanon/myInstagram/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
